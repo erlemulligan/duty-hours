bd38e62ecdf0c93415fda5120a23b9a89c19f70b
Erle Mulligan <erlemulligan@gmail.com>
2016-11-19 18:17:57 -0600

    separating css from the javascript bundle using ExtractTextPlugin

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,10 @@
 var webpack = require('webpack');
 var path = require('path');
 
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var extractCss = new ExtractTextPlugin('./assets/css/[name].css');
+
 var config = {
   srcPath: './app',
   bundlePath: './bundle/',
@@ -11,13 +15,34 @@ var config = {
 
 module.exports = {
   context: path.resolve(config.srcPath),
+
   entry: {
-    jsBundle: [config.jsPath + 'app.js'],
+    bundle: [config.jsPath + 'app.js', config.cssPath + 'app.css'],
   },
+
   output: {
     path: path.join(config.bundlePath),
     publicPath: config.publicPath,
     filename: "./assets/js/[name].js",
-  }
+  },
+
+  module: {
+    preLoaders: [
+
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: extractCss.extract('style', 'css'),
+      },
+    ],
+
+  },
+
+  plugins: [
+    extractCss
+  ]
+
 
 };
